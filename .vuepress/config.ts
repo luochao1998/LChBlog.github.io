5c8fe49cf7d0b0237377744437ccfc1385c23e4a
luochao <lch19981107@163.com>
2022-09-28 10:43:46 +0800

    first commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,44 @@
+import { defineUserConfig } from 'vuepress'
+import { recoTheme } from 'vuepress-theme-reco'
+
+export default defineUserConfig({
+  title: "午后南杂",
+  description: 'Enjoy when you can, and endure when you must.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+  ],
+  theme: recoTheme({
+    style: '@vuepress-reco/style-default',
+    logo: '/head.png',
+    // logo: '/hero_black.png',
+    author: 'reco_luan',
+    authorAvatar: '/head.png',
+    docsRepo: 'https://github.com/recoluan/recoluan.github.io',
+    docsBranch: 'gh-pages-source',
+    lastUpdatedText: '',
+    navbar: [
+      {
+        text: 'Contact',
+        children: [
+          { text: 'GitHub', link: 'https://github.com/recoluan' },
+        ]
+      }
+    ],
+    commentConfig: {
+      type: 'valine',
+      options: {
+        appId: 'Q6hMeY2PSaM9FMkXetzoJoU5-gzGzoHsz',
+        appKey: 'iLQlev5jo2Cm5pLcI0z3qhtr',
+        placeholder: '填写邮箱可以收到回复提醒哦！',
+        verify: true, // 验证码服务
+        // notify: true, //
+        recordIP: true,
+        // hideComments: true
+      },
+    },
+    autoAddCategoryToNavbar: true
+  }),
+  lang: 'zh-CN',
+})
