684d8bf83a096547c2436fc851467ca392149a8c
luochao1998 <95325523+luochao1998@users.noreply.github.com>
2023-02-20 10:11:20 +0000

    Deploying to gh-pages from @ luochao1998/LChBlog.github.io@d85e3c46cb31d9b3f7e7dc443a0ed0c0a964fb13 🚀

---
+++ assets/022001.html.6a2df5b7.js
@@ -0,0 +1,20 @@
+import{_ as n,o as s,c as a,a as t}from"./app.320f35c7.js";const e={},p=t(`<div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token operator">&lt;</span>div <span class="token keyword">class</span><span class="token operator">=</span><span class="token string">&quot;demo-loading&quot;</span><span class="token operator">&gt;</span>\u73A9\u547D\u52A0\u8F7D\u4E2D<span class="token operator">&lt;</span><span class="token operator">/</span>div<span class="token operator">&gt;</span>
+<span class="token operator">&lt;</span>style<span class="token operator">&gt;</span>
+<span class="token punctuation">.</span>demo<span class="token operator">-</span>loading<span class="token operator">:</span><span class="token operator">:</span>after <span class="token punctuation">{</span>
+  <span class="token literal-property property">content</span><span class="token operator">:</span> <span class="token string">&quot;&quot;</span><span class="token punctuation">;</span>
+  <span class="token literal-property property">animation</span><span class="token operator">:</span> dots 2s infinite <span class="token function">steps</span><span class="token punctuation">(</span><span class="token number">3</span><span class="token punctuation">,</span> start<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+@keyframes dots <span class="token punctuation">{</span>
+  <span class="token number">33.33</span><span class="token operator">%</span> <span class="token punctuation">{</span>
+    <span class="token literal-property property">content</span><span class="token operator">:</span> <span class="token string">&quot;.&quot;</span><span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+  <span class="token number">66.67</span><span class="token operator">%</span> <span class="token punctuation">{</span>
+    <span class="token literal-property property">content</span><span class="token operator">:</span> <span class="token string">&quot;..&quot;</span><span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+  <span class="token number">100</span><span class="token operator">%</span> <span class="token punctuation">{</span>
+    <span class="token literal-property property">content</span><span class="token operator">:</span> <span class="token string">&quot;...&quot;</span><span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+<span class="token operator">&lt;</span><span class="token operator">/</span>style<span class="token operator">&gt;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),o=[p];function l(c,r){return s(),a("div",null,o)}var u=n(e,[["render",l],["__file","022001.html.vue"]]);export{u as default};
